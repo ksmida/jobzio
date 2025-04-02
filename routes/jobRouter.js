469bb609c0d0437ca43195f58ecbd80a26be2e2c
Kevin <smidakevin@gmail.com>
2025-04-02 17:44:46 -0400

    Implement dynamic route parameter validation Added dynamic route parameter validation to check for invalid MongoDB Ids

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,5 +1,8 @@
 import { Router } from 'express'
-import { validateJobInput } from '../middleware/validationMiddleware.js'
+import {
+  validateJobInput,
+  validateIdParam,
+} from '../middleware/validationMiddleware.js'
 
 const router = Router()
 import {
@@ -14,8 +17,8 @@ import {
 router.route('/').get(getAllJobs).post(validateJobInput, createJob)
 router
   .route('/:id')
-  .get(getJob)
-  .patch(validateJobInput, updateJob)
-  .delete(deleteJob)
+  .get(validateIdParam, getJob)
+  .patch(validateIdParam, validateJobInput, updateJob)
+  .delete(validateIdParam, deleteJob)
 
 export default router
