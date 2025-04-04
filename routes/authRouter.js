a5a7ac25ad94d508a0163d6b98643900846e6668
Kevin <smidakevin@gmail.com>
2025-04-04 15:04:04 -0400

    add validation middleware to /register route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 const router = Router()
 import { login, register } from '../controllers/authController.js'
+import { validateRegisterInput } from '../middleware/validationMiddleware.js'
 
-router.post('/register', register)
+router.post('/register', validateRegisterInput, register)
 router.post('/login', login)
 
 export default router
