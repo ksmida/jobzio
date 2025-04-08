6b892c4e096ebd0f78f523c63a80c71e283b8bd5
Kevin <smidakevin@gmail.com>
2025-04-08 14:09:53 -0400

    implement password hashing for user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,17 @@
 import { StatusCodes } from 'http-status-codes'
 import User from '../models/UserModel.js'
+import { hashPassword } from '../utils/passwordUtils.js'
 
-// Register
+// Register a user. First user's role is always an admin.
 export const register = async (req, res) => {
+  const isFirstAccount = (await User.countDocuments()) === 0
+  req.body.role = isFirstAccount ? 'admin' : 'user'
+
+  const hashedPassword = await hashPassword(req.body.password)
+  req.body.password = hashedPassword
+
   const user = await User.create(req.body)
-  res.status(StatusCodes.CREATED).json({ user })
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' })
 }
 
 // Login
