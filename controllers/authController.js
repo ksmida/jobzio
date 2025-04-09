1984089ab7b641f7c0aa2ada8eae28f4c3cc4a83
Kevin <smidakevin@gmail.com>
2025-04-09 14:25:55 -0400

    implement httpOnly cookie when sending JWT When a JWT is generated, it is now sent back to the front-end via httpOnly cookie that has an expiration date of one day.

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,6 +27,12 @@ export const login = async (req, res) => {
   if (!isPasswordCorrect) throw new UnauthenticatedError('invalid credentials')
 
   const token = createJWT({ userId: user._id, role: user.role })
+  const oneDay = 1000 * 60 * 60 * 24
 
-  res.send({ token })
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  })
+  res.status(StatusCodes.OK).json({ msg: 'user logged in' })
 }
