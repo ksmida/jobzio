0cd4ff27e7ebc8c4c65f23c7e372e6f70496a302
Kevin <smidakevin@gmail.com>
2025-04-12 13:40:15 -0400

    associate created job with logged-in user Gave 'createJob' function the ability to populate 'req.body.createdBy' with 'req.user.userId',  ensuring each job is linked to the user who created it.

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -3,12 +3,13 @@ import { StatusCodes } from 'http-status-codes'
 
 // Fetch all jobs
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({})
+  const jobs = await Job.find({ createdBy: req.user.userId }) // jobs are populated based on user
   res.status(StatusCodes.OK).json({ jobs })
 }
 
 // Create a new job
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId
   const job = await Job.create(req.body)
   res.status(StatusCodes.CREATED).json({ job })
 }
