203ee99f6666f318b140b710dfdbff735727639e
Kevin <smidakevin@gmail.com>
2025-05-12 13:24:48 -0400

    replace form element with React Router's <Form> Replaced the standard form element with React Router's <Form> to promote separation of concerns. Added an action function to handle form submissions via a POST request, redirecting the user to the login page after form submission.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Form, redirect, useNavigation, Link } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
 import { FormRow, Logo } from '../components'
+import customFetch from '../utils/customFetch'
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+
+  try {
+    await customFetch.post('/auth/register', data)
+    return redirect('/login')
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+}
 
 const Register = () => {
   return (
     <Wrapper>
-      <form className='form'>
+      <Form method='post' className='form'>
         <Logo />
         <h4>Register</h4>
         <FormRow type='text' name='name' defaultValue='john' />
         <FormRow
-        type='text'
-        name='lastName'
-        labelText='last name'
-        defaultValue='smith'
+          type='text'
+          name='lastName'
+          labelText='last name'
+          defaultValue='smith'
         />
         <FormRow type='text' name='location' defaultValue='earth' />
         <FormRow type='email' name='email' defaultValue='john@gmail.com' />
@@ -28,7 +41,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   )
 }
